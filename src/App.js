07cc238a8fdc545657eeae430a5da036f851b736
Maximiliano Pereira <massi.laplata@gmail.com>
2022-07-17 01:42:32 -0300

    navbarrrr

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Navbar from './components/Navbar/Navbar.js';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Navbar />
+        {/* 
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -16,7 +19,7 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
       </header>
     </div>
   );
