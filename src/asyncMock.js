f26617127a45c41719fe0e22acefd8093ac5a4fb
Maximiliano Pereira <massi.laplata@gmail.com>
2022-07-25 16:30:45 -0300

    ContadorcondisablesyAgregarCarrito

---
+++ src/asyncMock.js
@@ -0,0 +1,12 @@
+const products = [
+    { 
+        id: '1', 
+        name: 'iphone 12', 
+        price: 1000, 
+        category: 'celular', 
+        img:'https://www.apple.com/newsroom/images/product/iphone/standard/Apple_announce-iphone12pro_10132020.jpg.og.jpg?202108061040', 
+        stock: 25, 
+        description:'Descripcion de Iphone 12'
+    },
+    { id: '2', name: 'samsung s21', price: 800, category: 'celular', img:'https://cdn.pocket-lint.com/r/s/1200x630/assets/images/155378-phones-review-hands-on-samsung-galaxy-s21-ultra-image1-luae09ici4.JPG', stock: 16, description:'Descripcion de Samsung s21'}
+]
--- src/components/Counter/Counter.js
+++ src/components/Counter/Counter.js
@@ -1,18 +1,19 @@
 import { useState } from 'react'
 import './Counter.css';
 
-const Counter = () => {
-    const [count, setCount] = useState(1)
+const Counter = ({stock, initial, onAdd }) => {
+    const [count, setCount] = useState(initial)
+    /* const [stock, setStock] = useState({stock}) */
 
     const decrement = () => {
 
-        if (count > 1) {
+        if (count > 0) {
             setCount(count - 1)
         }
     }
 
     const increment = () => {
-        if (count < 10) {
+        if (count < stock) {
             setCount(count + 1)
         }
 
@@ -21,23 +22,34 @@ const Counter = () => {
     return (
         <div className="counter">
             <h1>{count}</h1>
-            <div>
-                <button onClick={decrement} className="button-82-pushable" >
-                    <span className="button-82-shadow"></span>
-                    <span className="button-82-edge"></span>
-                    <span className="button-82-front text">
-                        -
-                    </span>
-                </button>
+            <div className="counter-contador">
+                
+                <div>
+                    <button disabled={count === 0 } onClick={decrement} className="button-82-pushable">
+                        <span className="button-82-shadow"></span>
+                        <span className="button-82-edge"></span>
+                        <span className="button-82-front text">
+                            -
+                        </span>
+                    </button>
+                </div>
+                <div>
+                    <button disabled={count === stock } onClick={increment} className="button-82-pushable">
+                        <span className="button-82-shadow"></span>
+                        <span className="button-82-edge"></span>
+                        <span className="button-82-front text">
+                            +
+                        </span>
+                    </button>
+                </div>
             </div>
+
             <div>
-                <button onClick={increment} className="button-82-pushable" >
-                    <span className="button-82-shadow"></span>
-                    <span className="button-82-edge"></span>
-                    <span className="button-82-front text">
-                        +
-                    </span>
+
+                <button disabled={count === 0 } onClick={() => onAdd(count)} className="button-64" >
+                    <span class="text">Agregar Al Carrito</span>
                 </button>
+
             </div>
 
         </div>
