6c85b3fe17acc11d010363de060ce10fdd02b46d
Maximiliano Pereira <massi.laplata@gmail.com>
2022-08-17 22:23:45 -0300

    4

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -63,7 +63,7 @@ const Cart = () => {
             </h1>
             {cart.map(prod => <CartProduct key={prod.id} {...prod}/>)}
             <h2>Total: U$DT {total}</h2>
-            <div className="confirmarCarrito ">
+            <div className="confirmarCarrito">
                 <button onClick={() => clearCart()} className="button-78 vaciarCarrito btns-cart">Vaciar Carrito</button>
                 <button onClick={createBuy} className="button-78 btn-confirmar btns-cart" >Confimar Compra</button>
             </div>
